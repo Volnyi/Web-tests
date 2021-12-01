3177dba82c67d7988a5cf951eeac139058b3b3aa
Markov <boratboroda@yandex.ru>
2021-12-01 18:55:53 +0300

    Правки по данным авторизации

--- data.py
+++ data.py
@@ -8,5 +8,5 @@ driver_chrome = os.path.abspath(os.curdir + '/drivers/chromedriver.exe')
 driver_firefox = os.path.abspath(os.curdir + '/drivers/geckodriver.exe')
 
 """data"""
-user_login = {'correct_login': 'alexander.markov@1cog.games', 'uncorrect_login': 'test@test.com'}
+user_login = {'correct_login': 'alexander.markov@test.test', 'uncorrect_login': 'test@test.com'}
 user_password = {'correct_password': '123123', 'uncorrect_password': 'qwerty'}
